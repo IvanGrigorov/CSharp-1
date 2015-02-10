4746c90b472470e61e8162a0b2b0644b4081b715
IvanGrigorov <ivangrigorov9@gmail.com>
2015-02-10 15:16:14 +0200

    Insert ConditionalStatements Project

---
+++ ConditionalStatements/05.TheBiggestof3Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.TheBiggestof3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter the first number. ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please, enter the second number. ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please, enter the third number. ");
+            double c = double.Parse(Console.ReadLine());           
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b > a && b > c )
+            {
+                Console.WriteLine(b);
+            }
+            else 
+            {
+                Console.WriteLine(c);
+            }           
+        }
+    }
+}
---
+++ ConditionalStatements/07.Sort3NumberswithNestedIfs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Sort3NumberswithNestedIfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Enter a:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter c:");
+            c = double.Parse(Console.ReadLine());
+
+            if (a >= b)
+                if (b >= c)
+                    Console.WriteLine(a + " " + b + " " + c);
+                else
+                    Console.WriteLine(c + " " + a + " " + b);
+            else if (b >= a)
+                if (a >= c)
+                    Console.WriteLine(b + " " + a + " " + c);
+                else
+                    Console.WriteLine(c + " " + b + " " + a);
+            else if (a >= c && c >= b)
+                Console.WriteLine(a + " " + c + " " + b);
+            else
+                Console.WriteLine(b + " " + c + " " + a);
+        }
+    }
+}
